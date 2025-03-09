c0f01d49a0413802fbcaad4ae57833c73eeed390
Misaka <misaka@yangrucheng.top>
2025-03-09 21:15:33 +0800

    fix: default config

---
+++ resource/config.js
@@ -0,0 +1,19 @@
+// See "https://github.com/misaka-org/Chaoxing-MiniProgram/blob/main/miniprogram/README.md" for more information.
+
+const config = {
+    baseUrl: "https://cx-proxy.example.com",
+    repository: "misaka-org/Chaoxing-WechatMiniProgram",
+
+    baiduMapKey: "", // 百度地图 API Key
+    tianMapKey: "", // 天地图 API Key
+
+    notice: "本小程序由公众号御坂网络Misaka免费提供，谨防倒卖！",
+
+    swiperList: [
+        "/static/swiper/1.png",
+        "/static/swiper/2.png",
+        "/static/swiper/3.png",
+        "/static/swiper/4.png",
+    ],
+}
+module.exports = config;
